962b427b53243a90c5c86ede5627a5d77fc29eaa
beekhoy <sadykov-mikail@mail.ru>
2023-07-30 01:26:34 +0300

    дз №3 - 3 семинар - работает

---
+++ C+/lesson3/seminar/homework/task№3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число и получите числа в кубе от 1 до введеного числа");
+
+int N = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i <= N; i++)
+{
+    Console.WriteLine(Math.Pow((i), 3));
+}
