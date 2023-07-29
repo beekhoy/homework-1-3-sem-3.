97d5f285e2d948eb25031f5da60a8ce0bc8b650b
beekhoy <sadykov-mikail@mail.ru>
2023-07-30 01:10:08 +0300

    ДЗ № 2 с 3-го семинара

---
+++ C+/lesson3/seminar/homework/task№2/Program.cs
@@ -0,0 +1,26 @@
+﻿double Distance3d(int x1,int x2,int y1,int y2,int z1,int z2)
+{
+    double distance = Math.Sqrt(Math.Pow((x1-x2), 2) + Math.Pow((y1-y2), 2) + Math.Pow((z1-z2), 2));
+    return distance;
+}
+
+
+
+
+
+
+
+
+Console.WriteLine("Введите данные о двух точках на 3д координатной плоскости, чтоб получить расстояние между ними.");
+
+int x1 = int.Parse(Console.ReadLine());
+int x2 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Расстояние между точками = {Distance3d}");
+
+
+
