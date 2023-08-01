786d9dd4f82c3b12a89ac6dfd15d9ff511b37c7a
beekhoy <sadykov-mikail@mail.ru>
2023-08-02 01:47:54 +0300

    второе задание с 3-го семинара

---
+++ C+/Lesson4/HomeWork/Задача-2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetSum(int N)
+{
+    string sum = N.ToString();
+    int answer = 0;
+    for (int i=0; i < sum.Length; i++)
+    {
+        answer = answer + sum[i];
+        Console.WriteLine(sum[i]);
+    }
+    return answer;
+}
+
+Console.WriteLine("Введите число");
+
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetSum(N));
